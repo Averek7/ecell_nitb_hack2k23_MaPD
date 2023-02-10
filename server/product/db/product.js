649a80cdec526306e0d5393a7b56c02dd10ce362
Mohit <mohit27agrawal@gmail.com>
2023-02-11 05:23:04 +0530

    search product api creayed

--- server/product/db/product.js
+++ server/product/db/product.js
@@ -7,4 +7,18 @@ const insertProductDb = (payload) => {
     return pool.query(query, [payload.mfgId, payload.productName, payload.brand, payload.measure, payload.quantity, payload.code]);
 }
 
-export {insertProductDb};
+const searchProductDb = (payload) => {
+    const query = `SELECT pi.id           as productId,
+    pi.product_name as productName,
+    pi.brand        as brand,
+    pi.code_number  as codeNumber,
+    pi.measure,
+    pi.quantity
+FROM product_info pi
+WHERE pi.product_name LIKE ?
+OR pi.brand LIKE ? AND pi.mfg_id = ?;`;
+
+return pool.query(query, [payload.query, payload.query, payload.userId]);
+}
+
+export {insertProductDb, searchProductDb};
