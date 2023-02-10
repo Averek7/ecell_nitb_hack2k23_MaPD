9447888a62e71ac2ab75d27f2a683d647ab81887
Mohit <mohit27agrawal@gmail.com>
2023-02-10 22:08:52 +0530

    basic backend

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// import "./connections/db.js"
+import express from "express"
+import dotenv from 'dotenv'
+import log from "./log.js"
+// import auth from './routes/auth.js';
+
+const app = express()
+const port = 5000
+
+dotenv.config()
+
+app.use(express.json())
+
+// app.use("/api/users", usersRoutes)
+
+app.get("/", (req, res) => {
+  log.debug([], "[request call")
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  log.debug([port], "[lesenting on port]")
+  //   console.log(`Example app listening on port ${port}`)
+})
