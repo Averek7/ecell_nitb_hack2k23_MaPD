00df497e3548aa8c41b19ddcb0425fab73fa42f1
dhruv <dapanchal2506@gmail.com>
2023-02-11 11:29:11 +0530

    Home page added

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,15 +5,70 @@
 import Layout from "../components/Layout";
 import Support from "@/components/Support";
 import Desc from "@/components/Desc";
+import { useState } from "react";
+
+
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+
+  const [productId, setProductId] = useState('')
+
+  const handleChange = (e) => {
+    setProductId({
+      // ...productId,
+      [e.target.name]: e.target.value
+    })
+    //    console.log(data)
+  }
+
+
   return (
-    
-      <Layout>
-        
-      </Layout>
-    
+
+    <Layout>
+      <div className="home-container">
+        <div className="home-card1">
+          <div className="home-lottie-card">
+
+
+          </div>
+          <div className="home-input-id">
+            <h2 className="home-h2">
+              Enter the product id to search for originality of a product and see the supply chain!
+            </h2>
+            <div className="home-textbox">
+              <input type="text" name="productId" id="" onChange={(e) => handleChange(e)} />
+            </div>
+            <button className="home-input-button" type="submit">Enter</button>
+
+          </div>
+        </div>
+
+        <div className="home-or">
+          --------------------  OR --------------------
+        </div>
+
+        <div className="home-card2">
+          <div className="home-lottie-card">
+
+
+          </div>
+
+          <div className="home-scan-box">
+              You can also Scan QR code to get the desired details
+          </div>
+
+          <button className="home-scan-button" type="submit">SCAN QR</button>
+
+        </div>
+
+
+
+      </div>
+
+    </Layout>
+
   );
 }
