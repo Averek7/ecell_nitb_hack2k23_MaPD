8af5460055ca7fe22315d24768274927112921e6
Mohit <mohit27agrawal@gmail.com>
2023-02-11 08:08:40 +0530

    search apis

---
+++ client/pages/searchproduct/index.js
@@ -0,0 +1,14 @@
+import AddProductForm from "@/components/AddProductForm";
+import Layout from "@/components/Layout";
+import SearchProduct from "@/components/SearchProduct";
+
+
+export default function Home() {
+   return (
+     <div>
+       <Layout>
+       <SearchProduct/>
+       </Layout>
+     </div>
+   );
+ }
