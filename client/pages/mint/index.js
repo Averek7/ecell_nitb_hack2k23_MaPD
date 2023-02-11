e9aebe5f44b76803b20a16af27c74a48a60cbb18
Averek <singhavanish2003@gmail.com>
2023-02-11 23:23:56 +0530

    loader-ipfs-fixes

--- client/pages/mint/index.js
+++ client/pages/mint/index.js
@@ -3,16 +3,19 @@ import Layout from "@/components/Layout";
 import { Web3Storage } from "web3.storage";
 import { create } from "ipfs-http-client";
 import { useDispatch, useSelector } from "react-redux";
-import { mintQR } from "@/redux/nftQr";
 import { setSuccess } from "@/redux/slices/success";
 import { setError } from "@/redux/slices/error";
 import InputBox from "@/components/InputBox";
+import Error from "@/components/Error";
+import Success from "@/components/Success";
+import Loader from "@/components/Loader";
 
 const projectId = "2LaElUcAr2SYK3KuPpor7Xlc5hB";
 const projectSecret = "0947f1f7854b4631c685a30c20e51d4d";
 
 function index() {
   const dispatch = useDispatch();
+  const [localLoading, setLocalLoading] = useState(false);
   const [data, setData] = useState({
     title: "",
     description: "",
@@ -23,7 +26,7 @@ function index() {
   const { walletAddress, signer, nftInstances } = useSelector(
     (state) => state.header
   );
-  const { instances } = useSelector((state) => state.header)
+  const { instances } = useSelector((state) => state.header);
 
   const handleChange = (e) => {
     setData({
@@ -34,14 +37,14 @@ function index() {
   };
 
   const addData = async () => {
-    let tokenId = await instances.getProductId()
+    let tokenId = await instances.getProductId();
     // console.log(Number(tokenId.toString()))
     let editedData = {
       ...data,
-      "tokenId": Number(tokenId.toString())
-    }
+      tokenId: Number(tokenId.toString()),
+    };
     // console.log(editedData);
-  }
+  };
 
   const handleClick = () => {
     const auth =
@@ -56,47 +59,50 @@ function index() {
         authorization: auth,
       },
     });
+    setLocalLoading(true);
     client
       .add(JSON.stringify(data))
       .then(async (res) => {
         console.log("result", `https://ipfs.io/ipfs/${res.path}`);
-        const dataIpfs = `https://ipfs.io/ipfs/${res.path}`;
+        const dataIpfs = `https://ipfs.io/ipfs/${res.path}}`;
         console.log("address", walletAddress);
         console.log("dataIPFS", dataIpfs);
 
         let qrNftTx = await nftInstances.safeMint(walletAddress, dataIpfs);
         console.log("Mining...", qrNftTx.hash);
-        // Status
+
+        setLocalLoading(true);
         let tx = await qrNftTx.wait();
-        // Loader
         console.log("Mined !", tx);
-
-        // let event = tx.event[0];
-        // let value = event.args[2];
-        // let tokenId = value.toNumber();
-
         console.log(
           `Mined, see transaction: https://mumbai.polygonscan.com/tx/${qrNftTx.hash}`
         );
+        setLocalLoading(false);
       })
       .catch((err) => {
         console.log("Error: ", err);
         dispatch(setError("Failed to generate IPFS link, Please retry"));
+        setLocalLoading(false);
       });
   };
 
   const nftUpload = (e) => {
+    e.preventDefault();
+    setLocalLoading(true);
+
     const nFile = e.target.files;
     storage
       .put(nFile)
       .then((res) => {
         console.log(res);
+        setLocalLoading(false);
         setData({
           ...data,
-          image: `https://ipfs.io/ipfs${res}/${nFile[0].name}`,
+          image: `https://ipfs.io/ipfs/${res}/${nFile[0].name}`,
         });
       })
       .catch((err) => {
+        setLocalLoading(false);
         dispatch(setError(err.message));
       });
     console.log(data.image);
@@ -105,20 +111,11 @@ function index() {
     <div>
       <Layout>
         <div className="authContainer">
-        <div className="authcenter">
-          <div className="txt">
-            <h1 style={{ margin: "10px 0px" }}>Mint Product Details</h1>
-          </div>
-          {/* <Error /> */}
-          
-            {/* <input
-              
-              name="image"
-              type="file"
-              value={data.imgInput}
-              onChange={nftUpload}
-              className="inputLabel "
-            /> */}
+          <div className="authcenter">
+            <div className="txt">
+              <h1 style={{ margin: "10px 0px" }}>Mint Product Details</h1>
+            </div>
+            <Error />
             <InputBox
               name="image"
               type="file"
@@ -126,19 +123,18 @@ function index() {
               value={data.imgInput}
               handleChange={nftUpload}
               placeholder="Item Name"
-              // disabled={localLoading}
+              disabled={localLoading}
             />
-          
+
             <InputBox
               name="title"
               title="Title"
               value={data.title}
               handleChange={handleChange}
               placeholder="Item Name"
-              // disabled={localLoading}
+              disabled={localLoading}
             />
-          
-          
+
             <label className="inputLabel">
               Description:
               <textarea
@@ -147,23 +143,18 @@ function index() {
                 value={data.description}
                 onChange={(e) => handleChange(e)}
                 placeholder="Provide detailed description of your item"
-                // disabled={localLoading}
+                disabled={localLoading}
               />
             </label>
-          
-          <div className="widthDiv">
-            <button className="btn mintBtn" onClick={addData}>
-              {/* {loading || localLoading ? (
-                <Loader height="25" width="25" />
-              ) : (
-                "Mint"
-              )} */}
-              Submit
-            </button>
+
+            <div className="widthDiv">
+              <button className="btn mintBtn" onClick={handleClick}>
+                {localLoading ? <Loader height="25" width="25" /> : "Mint"}
+              </button>
+            </div>
           </div>
         </div>
-        </div>
-        {/* <Success /> */}
+        <Success />
       </Layout>
     </div>
   );
--- client/pages/transferOwner/index.js
+++ client/pages/transferOwner/index.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { ethers } from "ethers";
 import { useSelector } from "react-redux";
 import nft_contract_address from "../../assets/contract_data/nftAddress.json";
 import nftABI from "../../assets/contract_data/nft.json";
 import Layout from "@/components/Layout";
+import InputBox from "@/components/InputBox";
+import Success from "@/components/Success";
+import Error from "@/components/Error";
 
 function index() {
   const { signer } = useSelector((state) => state.header);
-  let toAddress = "0xab7dc3e852B8AE47B149036e398aC9D46e61409f";
-  let tokenId = "1";
+  const [localLoading, setLocalLoading] = useState(false);
+
+  // let toAddress = "0xab7dc3e852B8AE47B149036e398aC9D46e61409f";
+  let toAddress = "0x7eff959E7D7fB6b9F3cDA78599966870929A7628";
+  let tokenId = "0";
+  const [data, setData] = useState({
+    toAddress: "",
+    tokenId: "",
+  });
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+    console.log(data);
+  };
+
   const handleClick = async () => {
     console.log(nft_contract_address, nftABI, signer);
+    setLocalLoading(false);
 
     const contract = new ethers.Contract(
       nft_contract_address.address,
       nftABI.abi,
       signer
     );
+    setLocalLoading(true);
     console.log(contract);
 
     const currentOwner = await contract.ownerOf(tokenId);
+    setLocalLoading(true);
     console.log(currentOwner);
 
     let qrNftTransfer = await contract.transferFrom(
@@ -27,9 +49,11 @@ function index() {
       toAddress,
       tokenId
     );
+    setLocalLoading(true);
     console.log(qrNftTransfer);
 
     await qrNftTransfer.wait();
+    setLocalLoading(false);
     console.log(
       `NFT${tokenId} Transfered from ${currentOwner} to ${toAddress}`,
       qrNftTransfer
@@ -39,9 +63,37 @@ function index() {
   return (
     <div>
       <Layout>
-        <button type="submit" onClick={handleClick}>
-          Transfer
-        </button>
+        <div className="authContainer">
+          <div className="authcenter">
+            <div className="txt">
+              <h1 style={{ margin: "10px 0px" }}>Transfer Details</h1>
+            </div>
+            <Error />
+            <InputBox
+              name="toAddress"
+              title="Transfer to Address"
+              value={data.toAddress}
+              handleChange={handleChange}
+              placeholder="Enter Transfer Address"
+              disabled={localLoading}
+            />
+            <InputBox
+              name="tokenID"
+              title="NFT Token ID"
+              value={data.tokenId}
+              handleChange={handleChange}
+              placeholder="Enter NFT Token ID"
+              disabled={localLoading}
+            />
+
+            <div className="widthDiv">
+              <button className="btn mintBtn" onClick={handleClick}>
+                {localLoading ? <Loader height="25" width="25" /> : "Transfer"}
+              </button>
+            </div>
+          </div>
+        </div>
+        <Success />
       </Layout>
     </div>
   );
