9af25aed90b1b1fe04d40b5977cb713114685579
dhruv <dapanchal2506@gmail.com>
2023-02-12 14:32:48 +0530

    Transactions api added and some client changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import log from "./log.js"
 import { verifyToken } from "./auth/controllers/auth.js"
 import auth from './auth/routes/index.js';
 import product from './product/routes/index.js'
+import transactions from './transactions/routes/index.js'
 import cors from "cors"
 
 const app = express()
@@ -26,6 +27,7 @@ app.get("/", verifyToken, (req, res) => {
 
 app.use("/auth", auth);
 app.use("/product", product);
+app.use("/transactions", transactions);
 
 app.listen(port, () => {
   log.debug([port], "[lesenting on port]")
