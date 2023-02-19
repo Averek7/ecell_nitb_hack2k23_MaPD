e5d649da9150986e6b220b91966bfe61004a5df3
Averek <singhavanish2003@gmail.com>
2023-02-19 10:23:52 +0530

    resolve-depoly-test1

--- client/next.config.js
+++ client/next.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     return config;
   },
-  swcMinify: true,
+  // swcMinify: true,
   // experimental: {
   //   forceSwcTransforms: true,
   // },
--- server/index.js
+++ server/index.js
@@ -1,30 +1,32 @@
-import express from "express"
-import dotenv from 'dotenv'
-import log from "./log.js"
-import { verifyToken } from "./auth/controllers/auth.js"
-import auth from './auth/routes/index.js';
-import product from './product/routes/index.js'
-import transactions from './transactions/routes/index.js'
-import mint from './mint/routes/index.js'
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import log from "./log.js";
+import { verifyToken } from "./auth/controllers/auth.js";
+import auth from "./auth/routes/index.js";
+import product from "./product/routes/index.js";
+import transactions from "./transactions/routes/index.js";
+import mint from "./mint/routes/index.js";
+import cors from "cors";
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-dotenv.config()
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.json())
-app.use(cors())
-app.use(cors({
-  origin: '*'
-}))
+app.use(express.json());
+app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", verifyToken, (req, res) => {
-  log.debug([], "[request call")
-  res.send("Hello World")
-})
+  log.debug([], "[request call");
+  res.send("Hello World");
+});
 
 app.use("/auth", auth);
 app.use("/product", product);
@@ -32,5 +34,5 @@ app.use("/transactions", transactions);
 app.use("/mint", mint);
 
 app.listen(port, () => {
-  log.debug([port], "[lesenting on port]")
-})
+  log.debug([port], "[lesenting on port]");
+});
