756c8ec6a488e93c8a731fdca31a961c0ba30213
Averek <singhavanish2003@gmail.com>
2023-02-25 23:09:40 +0530

    deploy-error-fixes

--- client/pages/collection.js/index.js
+++ client/pages/collection.js/index.js
@@ -1,34 +1,34 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "@/components/Layout";
 // import NftCard from "@/components/NftCard";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
 // import axios from "axios";
-import { getMyNfts } from "@/redux/slices/collection";
-import { useAccount } from "wagmi";
-import Loader from "@/components/Loader";
+// import { getMyNfts } from "@/redux/slices/collection";
+// import { useAccount } from "wagmi";
+// import Loader from "@/components/Loader";
 
 function Index() {
-  const dispatch = useDispatch();
-  // const [localLoading, setLocalLoading] = useState(false);
-  const { address } = useAccount();
-  const mynfts = useSelector((state) => state.collection);
+  // const dispatch = useDispatch();
+  // // const [localLoading, setLocalLoading] = useState(false);
+  // const { address } = useAccount();
+  // // const mynfts = useSelector((state) => state.collection);
 
-  useEffect(() => {
-    dispatch(getMyNfts(address));
-  }, [address, dispatch]);
+  // useEffect(() => {
+  //   dispatch(getMyNfts(address));
+  // }, [address, dispatch]);
 
   return (
     <Layout>
       <div className="collection-container">
         <div className="card-container">
           <>
-            {mynfts ? (
+            {/* {mynfts ? (
               mynfts.map(() => <></>)
             ) : (
               <div>
                 <Loader />
               </div>
-            )}
+            )} */}
           </>
         </div>
       </div>
